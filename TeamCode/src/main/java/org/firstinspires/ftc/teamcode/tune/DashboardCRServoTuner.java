8867a6843631d16865b168bd3ac18cce4d9bed98
Faizan Masroor <faizanmasroor01@gmail.com>
2025-11-09 17:56:21 -0600

    Add "by-name" tuner for CR servos (hasn't been tested either)ˆ

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tune/DashboardCRServoTuner.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode.tune;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.ValueProvider;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.CRServoImplEx;
+
+@TeleOp(group = "Tune")
+public class DashboardCRServoTuner extends OpMode {
+    @Override
+    public void init() {
+        FtcDashboard db = FtcDashboard.getInstance();
+
+        for (CRServoImplEx s : hardwareMap.getAll(CRServoImplEx.class)) {
+            s.setDirection(CRServo.Direction.FORWARD);
+            String name = hardwareMap.getNamesOf(s).iterator().next();
+            if (name == null) continue;
+
+            db.addConfigVariable(this.getClass().getSimpleName(), name, new ValueProvider<String>() {
+                final CRServoImplEx servo = s;
+                double pow = 0.5;
+                String verbatimInput = "";
+
+                @Override
+                public String get() {
+                    return verbatimInput;
+                }
+
+                @Override
+                public void set(String value) {
+                    if (value.equals("")) {
+                        this.servo.setPwmDisable();
+                    } else {
+                        try {
+                            this.pow = Double.parseDouble(value);
+                        } catch (NumberFormatException e) {
+                            return;
+                        }
+
+                        this.servo.setPwmEnable();
+                        this.servo.setPower(this.pow);
+                    }
+
+                    this.verbatimInput = value;
+                }
+            }, true);
+        }
+    }
+
+    @Override
+    public void loop() {}
+}
