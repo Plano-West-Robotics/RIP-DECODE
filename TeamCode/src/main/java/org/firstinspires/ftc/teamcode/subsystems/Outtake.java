22d2907bc9e8c52e444e5a1157fa8fe91c9fcad3
Faizan Masroor <faizanmasroor01@gmail.com>
2025-11-10 17:05:37 -0600

    Add calculateIdealVelocity() to Outtake classˆ

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Outtake.java
@@ -7,6 +7,11 @@ import org.firstinspires.ftc.teamcode.hardware.base.MotorWrapper;
 
 public class Outtake
 {
+    public static final double TICKS_PER_REV = 9.3333333333; // not been tested
+    public static final double HALF_GRAVITY = 4.9;
+    public static final double LAUNCH_ANGLE = Math.PI / 3;
+    public static final double DELTA_Y = 0.6345428; // = height of goal - height of outtake
+
     public static final double POWER = 0.55;
     public static final double TRIGGER_THRESHOLD = 0.5;
 
@@ -23,5 +28,18 @@ public class Outtake
     {
         isSpinning = gamepads.exceedsThreshold(Analog.GP1_RIGHT_TRIGGER, TRIGGER_THRESHOLD);
         motor.setPower(isSpinning ? POWER : 0);
+    }
+
+    /**
+     * @param dx meters
+     * @return
+     */
+    public double calculateIdealVelocity(double dx)
+    {
+        return Math.sqrt(
+            (-HALF_GRAVITY * Math.pow(dx, 2))
+            /
+            (Math.pow(Math.cos(LAUNCH_ANGLE), 2) * DELTA_Y - Math.sin(LAUNCH_ANGLE) * Math.cos(LAUNCH_ANGLE) * dx)
+        );
     }
 }
