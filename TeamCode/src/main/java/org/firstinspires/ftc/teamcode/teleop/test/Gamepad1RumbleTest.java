951945fcc68a850bdbe9239b7cf49fe143b2b9f3
Faizan Masroor <faizanmasroor01@gmail.com>
2025-12-05 19:04:41 -0600

    Add GP1 rumble test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/test/Gamepad1RumbleTest.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.teleop.test;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.core.control.Gamepads;
+import org.firstinspires.ftc.teamcode.teleop.BaseTeleOp;
+
+@Config
+@TeleOp(group = "Test")
+public class Gamepad1RumbleTest extends BaseTeleOp
+{
+    public Gamepads gamepads;
+    public static boolean rumbleGP1;
+    public boolean x = false;
+
+    @Override
+    public void setup()
+    {
+        gamepads = new Gamepads(gamepad1, gamepad2);
+    }
+
+    @Override
+    public void run()
+    {
+        if (rumbleGP1 && !gamepad1.isRumbling())
+        {
+            gamepad1.rumbleBlips(1);
+            x = true;
+        }
+
+        telemetry.addData("X", x);
+    }
+}
